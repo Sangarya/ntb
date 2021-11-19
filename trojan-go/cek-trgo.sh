f7237f65ddbe988bb06f68bc3bfed025b46dcdcb
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-11-19 19:46:25 +0800

    Add files via upload

--- trojan-go/cek-trgo.sh
+++ trojan-go/cek-trgo.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-datar=( `cat /var/log/trojan-go/trojan-go.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
-echo -e "${red}============================${NC}";
-echo -e "    TROJAN-GO USER LOGIN" | lolcat
-echo -e "${red}============================${NC}";
-for akuun in "${datar[@]}"
-do
-datat=( `lsof -n | grep -i ESTABLISHED | grep trojan-go | awk '{print $9}' | cut -d':' -f2 | grep -w 443 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
-echo -n > /tmp/iptrojan-go.txt
-for ip in "${datat[@]}"
-do
-jume=$(cat /var/log/trojan-go.log | grep -w $akun | awk '{print $4}' | cut -d: -f1 | grep -w $ip | sort | uniq)
-if [[ -z "$jume" ]]; then
-echo > /dev/null
-else
-echo "$jume" > /tmp/iptrojan-go.txt
-fi
-done
-jumo=$(cat /tmp/iptrojan-go.txt | nl)
-echo "User : $akuun";
-echo "$jumo";
-echo -e "${red}============================${NC}";
-done
+z="
+";Lz='r=( ';JCz='at /';OCz='awk ';Uz='o.lo';Ez='gree';oz='"${r';Sz='troj';NBz='f -n';KBz='do';QCz='int ';Hz='32m'\''';xz='GO U';Gz='e[0;';PCz=''\''{pr';FBz='akuu';RBz='STAB';HCz='jume';tz='";';wBz='mp/i';HBz=' "${';BBz='N" |';aCz='[ -z';Vz='g | ';nCz='txt';WBz='-go ';qz='====';Mz='`cat';Pz='g/tr';XBz='| aw';xBz='ptro';Dz='m'\''';oCz='fi';DCz='n "$';dBz=' -d'\''';dCz=' ]];';gCz=' > /';rCz='txt ';sCz='| nl';jBz='ut -';sBz=''\''>'\'' ';Fz='n='\''\';ez=' '\''{p';tCz=')';jz=' | u';eBz=':'\'' -';ECz='{dat';yBz='jan-';Oz='r/lo';Bz=''\''\e[';ADz='umo"';TCz='f1 |';ZCz='if [';oBz='.0.1';iCz='null';ABz='LOGI';fBz='f2 |';RCz='$4}'\''';az='icat';dz=' awk';VBz='p tr';SCz='d: -';yCz=';';ZBz='prin';lz=');';hBz=' 443';fCz='n';MCz='$aku';YBz='k '\''{';eCz=' the';Iz='NC='\''';cBz=' cut';SBz='LISH';vz=' TRO';OBz=' | g';Nz=' /va';CBz=' lol';Qz='ojan';iBz=' | c';mCz='-go.';WCz='ort ';qBz='| cu';CCz='ip i';Cz='1;31';uz='"   ';rz='===$';TBz='ED |';hz=''\'' | ';LBz='t=( ';lCz='iptr';wCz=' $ak';kCz='tmp/';wz='JAN-';Yz=''\''aut';UCz=' $ip';sz='{NC}';uCz=' "Us';Rz='-go/';Jz='\e[0';bz='ed a';Zz='hent';YCz='iq)';QBz='-i E';BCz='xt';Wz='grep';Az='red=';fz='rint';LCz='log/';aBz='t $9';ACz='go.t';uBz=' -n ';DBz='cat';MBz='`lso';hCz='dev/';qCz='jumo';Kz='data';cCz='ume"';Tz='an-g';pCz='done';ICz='=$(c';nBz='27.0';mBz=' '\''>1';rBz='t -d';iz='sort';GCz=']}"';vBz='> /t';cz='s'\'' |';bBz='}'\'' |';GBz='n in';XCz='| un';bCz=' "$j';vCz='er :';lBz='p -v';pz='ed}=';gz=' $7}';UBz=' gre';jCz='else';kz='niq`';EBz='for ';xCz='uun"';Xz=' -w ';IBz='r[@]';pBz='niq ';kBz='d- -';NCz='n | ';yz='SER ';FCz='at[@';VCz=' | s';tBz='-f2`';JBz='}"';mz='echo';KCz='var/';nz=' -e ';PBz='rep ';gBz='p -w';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Dz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$z$mz$nz$oz$pz$qz$qz$qz$qz$qz$qz$rz$sz$tz$z$mz$nz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$z$mz$nz$oz$pz$qz$qz$qz$qz$qz$qz$rz$sz$tz$z$EBz$FBz$GBz$HBz$Kz$IBz$JBz$z$KBz$z$Kz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$Qz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$eBz$fBz$UBz$gBz$hBz$iBz$jBz$kBz$fBz$UBz$lBz$mBz$nBz$oBz$hz$iz$jz$pBz$qBz$rBz$sBz$tBz$lz$z$mz$uBz$vBz$wBz$xBz$yBz$ACz$BCz$z$EBz$CCz$DCz$ECz$FCz$GCz$z$KBz$z$HCz$ICz$JCz$KCz$LCz$Sz$Tz$Uz$Vz$Wz$Xz$MCz$NCz$OCz$PCz$QCz$RCz$iBz$jBz$SCz$TCz$UBz$gBz$UCz$VCz$WCz$XCz$YCz$z$ZCz$aCz$bCz$cCz$dCz$eCz$fCz$z$mz$gCz$hCz$iCz$z$jCz$z$mz$bCz$cCz$gCz$kCz$lCz$Qz$mCz$nCz$z$oCz$z$pCz$z$qCz$ICz$JCz$kCz$lCz$Qz$mCz$rCz$sCz$tCz$z$mz$uCz$vCz$wCz$xCz$yCz$z$mz$bCz$ADz$yCz$z$mz$nz$oz$pz$qz$qz$qz$qz$qz$qz$rz$sz$tz$z$pCz"
--- trojan-go/port-trgo.sh
+++ trojan-go/port-trgo.sh
@@ -1,33 +1,3 @@
-#!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- ifconfig.me/ip);
-echo "Checking VPS"
-clear
-tr="$(cat ~/log-install.txt | grep -i Trojan-go | cut -d: -f2|sed 's/ //g')"
-echo -e "Name : Change Port Trojan GO"
-echo -e "============================" | lolcat
-echo -e "Change Port $tr"
-read -p "New Port Trojan-go: " tr2
-if [ -z $tr2 ]; then
-echo "Please Input Port"
-exit 0
-fi
-cek=$(netstat -nutlp | grep -w $tr2)
-if [[ -z $cek ]]; then
-sed -i "s/$tr/$tr2/g" /etc/trojan-go/config.json
-sed -i "s/   - Trojan                  : $tr/   - Trojan-go                  : $tr2/g" /root/log-install.txt
-iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $tr -j ACCEPT
-iptables -D INPUT -m state --state NEW -m udp -p udp --dport $tr -j ACCEPT
-iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $tr2 -j ACCEPT
-iptables -I INPUT -m state --state NEW -m udp -p udp --dport $tr2 -j ACCEPT
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save > /dev/null
-netfilter-persistent reload > /dev/null
-systemctl restart trojan-go > /dev/null
-echo -e "\e[032;1mPort $tr2 modified successfully\e[0m"
-else
-echo "Port $tr2 is used"
-fi
+z="
+";bz='$(ca';Xz='PS"';iCz='m tc';dDz='emct';OBz=' -p ';UBz='r2';pDz='cess';aDz='ev/n';aCz=' -D ';PCz='- Tr';az='tr="';sBz=' $ce';bDz='ull';pCz='PT';kz='ojan';tBz='k ]]';wCz='-sav';Lz='=$(w';yCz='/etc';FCz='n-go';WBz=' -z ';EBz='====';JDz='tc/i';WCz='tall';rz='/ //';QBz=' Por';rDz='y\e[';NCz='   :';hz='| gr';hDz='"\e[';ZBz='then';CBz='O"';gDz='t tr';oBz='rep ';lBz='t -n';tCz='2 -j';ICz='json';bBz='ease';fBz='exit';nCz=' -j ';JCz='s/  ';lDz='tr2 ';DBz='"===';rCz=' udp';SBz='-go:';YDz='oad ';LCz='n   ';xCz='e > ';RDz='rsis';KDz='ptab';sz='g'\'')"';cDz='syst';Oz=' ifc';Rz='/ip)';eBz='ort"';TDz=' sav';Pz='onfi';Sz=';';cz='t ~/';PDz='ilte';KCz=' - T';HCz='fig.';CDz='s.up';kBz='tsta';fDz='star';Wz='ng V';HBz='cat';ACz='r/$t';nBz=' | g';kCz=' tcp';lz='-go ';FDz='-res';pz='2|se';JBz='nge ';ZCz='bles';jBz='$(ne';MDz='up.r';Iz='NC='\''';iBz='cek=';TBz=' " t';wz='Chan';Mz='get ';mz='| cu';wDz='sed"';Nz='-qO-';MCz='    ';VBz='if [';vDz='is u';HDz=' -t ';BDz='able';pBz='-w $';Gz='e[0;';oz=': -f';hBz='fi';DDz='.rul';FBz='=" |';fz='all.';Az='red=';SDz='tent';cCz='T -m';dz='log-';oCz='ACCE';xBz='-i "';Ez='gree';oDz=' suc';uCz=' ACC';mCz='port';jz='i Tr';wBz='sed ';bCz='INPU';XBz='$tr2';ODz='netf';Jz='\e[0';DCz='tc/t';Vz='ecki';LDz='les.';dCz=' sta';ez='inst';iDz='032;';CCz='" /e';qDz='full';qz='d '\''s';XCz='.txt';hCz='EW -';WDz='l';eDz='l re';yBz='s/$t';sDz='0m"';ZDz='> /d';Dz='m'\''';vBz='en';fCz='-sta';IDz='< /e';OCz='/   ';IBz='"Cha';tDz='else';Zz='r';cBz=' Inp';LBz=' $tr';TCz='root';Yz='clea';QDz='r-pe';uz='"Nam';SCz='g" /';gz='txt ';GDz='tore';rBz='[ -z';UCz='/log';ECz='roja';NDz='ules';GBz=' lol';YBz=' ]; ';vCz='EPT';NBz='read';tz=' -e ';RCz='tr2/';qBz='tr2)';dBz='ut P';kDz='rt $';QCz=' : $';nz='t -d';Qz='g.me';BBz='an G';uDz=' "Po';YCz='ipta';ADz='/ipt';xz='ge P';VDz='/nul';iz='ep -';mBz='utlp';Kz='MYIP';Tz='echo';jDz='1mPo';Uz=' "Ch';UDz='/dev';gCz='te N';Hz='32m'\''';jCz='p -p';VCz='-ins';vz='e : ';RBz='t Tr';qCz='m ud';yz='ort ';lCz=' --d';KBz='Port';XDz=' rel';sCz=' -I ';eCz='te -';MBz='"';EDz='es';Fz='n='\''\';gBz=' 0';Cz='1;31';ABz='Troj';GCz='/con';nDz='fied';PBz='"New';uBz='; th';aBz=' "Pl';BCz='r2/g';mDz='modi';Bz=''\''\e[';
+eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Dz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$Xz$z$Yz$Zz$z$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$z$Tz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$z$Tz$tz$DBz$EBz$EBz$EBz$EBz$EBz$EBz$FBz$GBz$HBz$z$Tz$tz$IBz$JBz$KBz$LBz$MBz$z$NBz$OBz$PBz$QBz$RBz$kz$SBz$TBz$UBz$z$VBz$WBz$XBz$YBz$ZBz$z$Tz$aBz$bBz$cBz$dBz$eBz$z$fBz$gBz$z$hBz$z$iBz$jBz$kBz$lBz$mBz$nBz$oBz$pBz$qBz$z$VBz$rBz$sBz$tBz$uBz$vBz$z$wBz$xBz$yBz$ACz$BCz$CCz$DCz$ECz$FCz$GCz$HCz$ICz$z$wBz$xBz$JCz$KCz$ECz$LCz$MCz$MCz$MCz$NCz$LBz$OCz$PCz$kz$lz$MCz$MCz$MCz$MCz$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$z$YCz$ZCz$aCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$iCz$jCz$kCz$lCz$mCz$LBz$nCz$oCz$pCz$z$YCz$ZCz$aCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$qCz$jCz$rCz$lCz$mCz$LBz$nCz$oCz$pCz$z$YCz$ZCz$sCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$iCz$jCz$kCz$lCz$mCz$LBz$tCz$uCz$vCz$z$YCz$ZCz$sCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$qCz$jCz$rCz$lCz$mCz$LBz$tCz$uCz$vCz$z$YCz$ZCz$wCz$xCz$yCz$ADz$BDz$CDz$DDz$EDz$z$YCz$ZCz$FDz$GDz$HDz$IDz$JDz$KDz$LDz$MDz$NDz$z$ODz$PDz$QDz$RDz$SDz$TDz$xCz$UDz$VDz$WDz$z$ODz$PDz$QDz$RDz$SDz$XDz$YDz$ZDz$aDz$bDz$z$cDz$dDz$eDz$fDz$gDz$kz$lz$ZDz$aDz$bDz$z$Tz$tz$hDz$iDz$jDz$kDz$lDz$mDz$nDz$oDz$pDz$qDz$rDz$sDz$z$tDz$z$Tz$uDz$kDz$lDz$vDz$wDz$z$hBz"
