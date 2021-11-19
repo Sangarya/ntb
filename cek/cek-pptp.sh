67080d5ddf6dd8b840c398e5d1f8e4422f107816
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-11-19 12:56:26 +0800

    Add files via upload

--- cek/cek-pptp.sh
+++ cek/cek-pptp.sh
@@ -14,5 +14,3 @@ cat /tmp/login-db-pptp.txt
 echo " "
 echo -e "${red}============================${NC}";
 echo " ";
-z='b-pp';vz='USER';ez='in-d';Vz='{pri';Tz=' | a';Zz='- " ';hz='xt;';Iz='NC='\''';Hz='32m'\''';HBz=' lol';OBz='cat ';xz='IN" ';Mz='last';Pz='ppp ';lz='"${r';rz=' "  ';gz='tp.t';QBz=';';PBz='xt';Oz='rep ';Xz=' ",$';
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Dz$z$Kz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$z$iz$jz$z$iz$kz$lz$mz$nz$nz$nz$nz$nz$nz$oz$pz$qz$z$iz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$z$iz$kz$lz$mz$nz$nz$nz$nz$nz$nz$oz$pz$qz$z$iz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$z$iz$kz$JBz$KBz$LBz$MBz$MBz$MBz$MBz$MBz$MBz$MBz$MBz$NBz$pz$qz$z$OBz$cz$dz$ez$fz$gz$PBz$z$iz$jz$z$iz$kz$lz$mz$nz$nz$nz$nz$nz$nz$oz$pz$qz$z$iz$jz$QBz"
--- cek/cek-sstp.sh
+++ cek/cek-sstp.sh
@@ -7,5 +7,3 @@ accel-cmd show sessions
 echo ""
 echo -e "============================" | lolcat
 echo "- Mod By GEO GABUT"
-';bz='Mod ';Ez='gree';Iz='NC='\''';Tz=' ""';ez='ABUT';Qz='essi';Mz='acce';Rz='ons';Gz='e[0;';Pz='ow s';Vz='"===';Oz='d sh';Lz='r';Fz='n='\''\';Uz=' -e ';Az='red=';
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$z$Iz$Jz$Dz$z$Kz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$z$Sz$Uz$Vz$Wz$Wz$Wz$Wz$Wz$Wz$Xz$Yz$Zz$z$Sz$az$bz$cz$dz$ez$fz"
