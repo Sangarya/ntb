d868e581a7777cf70c15d191bad7095118305739
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-11-27 17:37:08 +0800

    Update trojaan.sh

--- menu/trojaan.sh
+++ menu/trojaan.sh
@@ -19,13 +19,13 @@ exit 0
 fi
 clear
 cat /usr/bin/bannerTROJAN | lolcat
-echo -e "${color1}1${color3}.$bd Create Trojan Account (${color2}ADD-TR${color3})"
-echo -e "${color1}2${color3}.$bd Deleting Trojan Account (${color2}DEL-TR${color3})"
-echo -e "${color1}3${color3}.$bd Renew Trojan Account (${color2}RENEW-TR${color3})"
-echo -e "${color1}4${color3}.$bd Check User Login Trojan (${color2}CEK-TR${color3})"
-echo -e "${color1}5${color3}.$bd Create TrojanGO Account (${color2}ADD-TRGO${color3})"
-echo -e "${color1}6${color3}.$bd Deleting TrojanGO Account (${color2}DEL-TRGO${color3})"
-echo -e "${color1}7${color3}.$bd Renew TrojanGO Account (${color2}RENEW-TRGO${color3})"
+echo -e "${color1}1${color3}.$bd Create Trojan Account (${color2}add-tr${color3})"
+echo -e "${color1}2${color3}.$bd Deleting Trojan Account (${color2}del-tr${color3})"
+echo -e "${color1}3${color3}.$bd Renew Trojan Account (${color2}renew-tr${color3})"
+echo -e "${color1}4${color3}.$bd Check User Login Trojan (${color2}cek-tr${color3})"
+echo -e "${color1}5${color3}.$bd Create TrojanGO Account (${color2}add-trgo${color3})"
+echo -e "${color1}6${color3}.$bd Deleting TrojanGO Account (${color2}del-trgo${color3})"
+echo -e "${color1}7${color3}.$bd Renew TrojanGO Account (${color2}renew-trgo${color3})"
 echo -e""
 echo -e "${color1}x${color3}.$bd Menu"
 echo -e""
